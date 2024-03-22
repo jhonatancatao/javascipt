a42ea5f802db81dd7403191314cca9f3985621d8
Jhonatan Catão <jhonatan10catao@hotmail.com>
2024-03-22 11:11:22 -0300

    Initial commit

---
+++ exercícios/ex´s feitos juntos/ex007/ex007.js
@@ -0,0 +1,6 @@
+let vel = 70
+console.log(`A velocidade do seu veículo é ${vel}Km/h`)
+if (vel > 60) { //condição simples!
+    console.log (`Você ultrapassou o limite de velocidade. MULTADO`)
+}
+console.log (`Dirija sempre com cinto de segurança!`)
