475b25cab7a0789dc3ee7c7e37db0181398770b0
Jhonatan Catão <jhonatan10catao@hotmail.com>
2024-04-19 16:15:58 -0300

    Update script.js

--- exercícios/ex´s feitos juntos/ex011/script.js
+++ exercícios/ex´s feitos juntos/ex011/script.js
@@ -3,6 +3,8 @@ function verificar() {
     let ano = data.getFullYear()
     let fano = document.getElementById('txtano')
     let res = document.getElementById('res')
+
+    
     if (fano.value.length == 0 || fano.value > ano) {
         window.alert('[ERRO] Verifique os dados e tente novamente!')
     } else {
